af00199246ceb87611aa3944872d4b9daeedcc10
Sumit Nema <developer.sumitnema@gmail.com>
2025-11-13 22:23:10 +0530

    Fix import case for Section1 component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-import Section1 from './components/section1';
+import Section1 from './components/Section1';
 import Section2 from './components/Section2';
 import Section3 from './components/Section3';
 import Section4 from "./components/Section4";
